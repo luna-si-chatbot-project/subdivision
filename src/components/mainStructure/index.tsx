c674cd2b1d917a711d93335bb6134d7924451a0d
Kerem-YounhoeSong <yhsong@lunasoft.co.kr>
2021-02-10 14:24:51 +0900

    feat: mainStructure 추가

---
+++ src/components/mainStructure/index.tsx
@@ -0,0 +1,23 @@
+import React, { ReactChild, ReactChildren } from "react";
+import Header from "../../components/header";
+import { sideBar } from "../../utils/sideBar";
+import Footer from "../footer";
+import { SideBar } from "../sidebar/sideBar";
+
+interface IChildren {
+  children: ReactChild;
+}
+const MainStructure = ({ children }: IChildren) => {
+  return (
+    <main className="flex">
+      <SideBar />
+      <section className="p-2 bg-gray-200 w-full">
+        <Header />
+        {children}
+        <Footer />
+      </section>
+    </main>
+  );
+};
+
+export default MainStructure;
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -5,6 +5,7 @@ import Header from "../components/header";
 import { SideBar } from "../components/sidebar/sideBar";
 import Pagination from "../components/pagination";
 import Footer from "../components/footer";
+import MainStructure from "../components/mainStructure";
 
 const TEST_QUERY = gql`
   query {
@@ -43,76 +44,75 @@ const ProjectPage = () => {
   const reverse = users.slice().sort((a: IUser, b: IUser) => b.id - a.id);
 
   return (
-    <main>
-      <div id="projectPage" className="flex">
-        <SideBar />
-        <section className="p-2 bg-gray-200 w-full">
-          <Header />
-          <div className="projectWrap bg-white mt-2">
-            <h3 className="px-5 py-3 bg-gray-600 text-white">프로젝트 관리</h3>
-            <div className="proejctInputWrap flex m-5 bg-white">
-              <div className="projectMakeWrap flex justify-center items-center">
-                <div className="px-3 bg-gray-100">
-                  <p>프로젝트 생성</p>
-                </div>
-                <div className="pl-3">
-                  <input
-                    className="border border-1 border-gray-100"
-                    placeholder="생성할 프로젝트 입력"
-                  />
-                </div>
-                <button className="px-3 border border-gray-400">생성</button>
-              </div>
-              <div className="projectSearchWrap flex justify-center items-center pl-24">
-                <div className="border border-1 border-gray-100">
-                  <input placeholder="이름이나 연락처로 검색해주세요"></input>
-                </div>
-                <button className="px-3 border border-1 bg-gray-700 text-white">
-                  검색
-                </button>
-              </div>
+    // <main>
+    //   <div id="projectPage" className="flex">
+    //     <SideBar />
+    //     <section className="p-2 bg-gray-200 w-full">
+    //       <Header />
+    <MainStructure>
+      <div className="projectWrap bg-white mt-2">
+        <h3 className="px-5 py-3 bg-gray-600 text-white">프로젝트 관리</h3>
+        <div className="proejctInputWrap flex m-5 bg-white">
+          <div className="projectMakeWrap flex justify-center items-center">
+            <div className="px-3 bg-gray-100">
+              <p>프로젝트 생성</p>
             </div>
-            <div className="projectListWrap p-5 devide-y devide-lightblue-500">
-              <table className="border-t border-1">
-                <thead className="border-b border-1">
-                  <tr>
-                    <th>NO</th>
-                    <th>프로젝트명</th>
-                    <th>담당자 연락처</th>
-                    <th>아이디</th>
-                    <th>패스워드</th>
-                    <th>홈페이지URL</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {reverse.map((user: IUser) => {
-                    return (
-                      <tr
-                        className="border-b border-1 text-center"
-                        key={user.id}
-                      >
-                        <td>{user.id}</td>
-                        <td>{user.name}</td>
-                        <td>{user.name}</td>
-                        <td>{user.id}</td>
-                        <td>1234</td>
-                        <td>www.lunasoft.co.kr</td>
-                        <td>
-                          <button>확인</button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+            <div className="pl-3">
+              <input
+                className="border border-1 border-gray-100"
+                placeholder="생성할 프로젝트 입력"
+              />
             </div>
-            <Pagination totalPage={3} />
+            <button className="px-3 border border-gray-400">생성</button>
           </div>
-          <Footer />
-        </section>
+          <div className="projectSearchWrap flex justify-center items-center pl-24">
+            <div className="border border-1 border-gray-100">
+              <input placeholder="이름이나 연락처로 검색해주세요"></input>
+            </div>
+            <button className="px-3 border border-1 bg-gray-700 text-white">
+              검색
+            </button>
+          </div>
+        </div>
+        <div className="projectListWrap p-5 devide-y devide-lightblue-500">
+          <table className="border-t border-1">
+            <thead className="border-b border-1">
+              <tr>
+                <th>NO</th>
+                <th>프로젝트명</th>
+                <th>담당자 연락처</th>
+                <th>아이디</th>
+                <th>패스워드</th>
+                <th>홈페이지URL</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {reverse.map((user: IUser) => {
+                return (
+                  <tr className="border-b border-1 text-center" key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.name}</td>
+                    <td>{user.id}</td>
+                    <td>1234</td>
+                    <td>www.lunasoft.co.kr</td>
+                    <td>
+                      <button>확인</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <Pagination totalPage={3} />
       </div>
-    </main>
+    </MainStructure>
+    //       <Footer />
+    //     </section>
+    //   </div>
+    // </main>
   );
 };
 
